24c5c1637305db7d61fa6d6389cd45ba04037fcf
sabir546 <mdgulamsabir9@gmail.com>
2023-08-06 15:55:17 +0530

    upto page6 completed with gsap and scrolltrigger

--- index.js
+++ index.js
@@ -1,3 +1,47 @@
+
+
+
+
+
+
+
+
+function cdn() {
+    gsap.registerPlugin(ScrollTrigger);
+
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true
+});
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy("#main", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+});
+
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.refresh();
+
+}
+cdn();
+
+
+
 var tl= gsap.timeline()
 tl.from("#page1 svg",{
     y:-50,
@@ -26,10 +70,10 @@ delay:-.3,
 
 gsap.to("#page1 svg",{
     scale:.1,
-    marginTop:"-11%",
+    marginTop:"-7.6%",
     scrollTrigger:{
 trigger:"#page1 svg ",
-scroller:"body",
+scroller:"#main",
 // markers:true,
 start:"top -0%",    
 end:"top -6%",
@@ -39,13 +83,13 @@ scrub:1,
 
 gsap.to("#page1 img",{
     
-y:500,
+y:150,
     scrollTrigger:{
 trigger:"#page1 img ",
-scroller:"body",
+scroller:"#main",
 // markers:true,
 start:"top -0%",    
-end:"top -100%",
+end:"top -60%",
 scrub:2,
     }
 })
@@ -87,10 +131,10 @@ gsap.to("#page2 h2 span",{
     stagger:.1,
 scrollTrigger:{
     trigger:"#page2 h2 span",
-    scroller:"body",
+    scroller:"#main",
     // markers:true,
     start:"top 60%",
-    end:"top -100%",
+    end:"top -80%",
     scrub:true,
 }
 })
@@ -100,7 +144,7 @@ gsap.to("#page2 #svg2 , #page2 #svg3",{
     left:"-5vw",
     scrollTrigger:{
         trigger:"#page2 #svg2",
-        scroller:"body",
+        scroller:"#main",
         // markers:true,
         start:"top 85%",
         end:"top -80%",
@@ -125,7 +169,7 @@ gsap.to("#page3 h2 span",{
     stagger:.1,
 scrollTrigger:{
     trigger:"#page3 h2 span",
-    scroller:"body",
+    scroller:"#main",
     // markers:true,
     start:"top 70%",
     end:"top 40%",
@@ -138,7 +182,7 @@ tl.from(" #page3 #lf p ,#page3 #rt #img1 ",{
     y:90,
     scrollTrigger:{
         trigger:" #page3 #lf p , #page3 #rt #img1 ",
-        scroller:"body",
+        scroller:"#main",
         // markers:true,
         start:"top 95%",
         end:"top 90%",
@@ -152,7 +196,7 @@ tl.from("#page3 #lf #arrow",{
    y:90,
    scrollTrigger:{
     trigger:" #page3 #lf #arrow",
-    scroller:"body",
+    scroller:"#main",
     // markers:true,
     start:"top 105%",
     end:"top 95%",
@@ -164,7 +208,7 @@ tl.from("#page3 #lf #img1",{
     y:90,
     scrollTrigger:{
      trigger:" #page3 #lf #img1",
-     scroller:"body",
+     scroller:"#main",
     //  markers:true,
      start:"top 105%",
      end:"top 95%",
@@ -178,10 +222,125 @@ tl.from("#page3 #lf #img1",{
     y:90,
     scrollTrigger:{
      trigger:" #page3 #rt #img2",
-     scroller:"body",
+     scroller:"#main",
     //  markers:true,
      start:"top 105%",
      end:"top 95%",
      scrub:2,
  }
  })
+
+
+ var swiper = new Swiper(".mySwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+      type: "fraction",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+
+
+  gsap.to("#page5 #svg5 , #page5 #svg6",{
+    left:"-25vw",
+    scrollTrigger:{
+        trigger:"#page5 #svg5, #page5 #svg6",
+        scroller:"#main",
+        // markers:true,
+        start:"top 85%",
+        end:"top -70%",
+        scrub:2,
+    }
+})
+
+
+
+
+var h2Data = document.querySelectorAll("#page6 h2")
+h2Data.forEach(function (elem) {
+var textData = elem.textContent;
+var splitedText = textData.split("")
+var clutter=""
+splitedText.forEach(function(e) {
+   clutter  += `<span>${e}</span>`
+});
+elem.innerHTML=clutter;
+});
+
+gsap.to("#page6 h2 span",{
+    color:"#ffff",
+    stagger:.1,
+scrollTrigger:{
+    trigger:"#page6 h2 span",
+    scroller:"#main",
+    // markers:true,
+    start:"top 65%",
+    end:"top 30%",
+    scrub:2,
+}
+})
+
+gsap.from("#page6  p",{
+    y:70,
+    opacity:0,
+scrollTrigger:{
+    trigger:"#page6 p",
+    scroller:"#main",
+    // markers:true,
+    start:"top 85%",
+    end:"top 80%",
+    scrub:2,
+}
+})
+
+gsap.from("#page6  #pprt1 img,#page6 #pprt3 img",{
+    y:50,
+    opacity:0,
+scrollTrigger:{
+    trigger:"#page6  #pprt1 img,#page6 #pprt3 img",
+    scroller:"#main",
+    // markers:true,
+    start:"top 95%",
+    end:"top 85%",
+    scrub:2,
+}
+})
+gsap.from("#page6  #pprt2 img",{
+    y:40,
+    opacity:0,
+scrollTrigger:{
+    trigger:"#page6  #pprt2 img",
+    scroller:"#main",
+    markers:true,
+    start:"top 100%",
+    end:"top 97%",
+    scrub:2,
+}
+})
+gsap.from("#page6  #arrow2",{
+    y:60,
+    opacity:0,
+scrollTrigger:{
+    trigger:"#page6  #arrow2",
+    scroller:"#main",
+    // markers:true,
+    start:"top 100%",
+    end:"top 101%",
+    scrub:2,
+}
+})
+
+gsap.to("#page6 #svg7 , #page6 #svg8",{
+    left:"-25vw",
+    scrollTrigger:{
+        trigger:"#page6 #svg7, #page6 #svg8",
+        scroller:"#main",
+        // markers:true,
+        start:"top 85%",
+        end:"top -70%",
+        scrub:2,
+    }
+})
